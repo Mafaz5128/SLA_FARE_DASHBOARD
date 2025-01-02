bb89a034f3df0d66c7dfdba63095e4ca450c227d
Mafaz Manzil <90832527+Mafaz5128@users.noreply.github.com>
2025-01-02 15:54:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.graph_objects as go
 import numpy as np
-
+from sklearn.linear_model import LinearRegression
 # Set page configuration to optimize layout
 st.set_page_config(layout="wide")
 
