560952dc22c3d60b4b2e9eb89ce1b268284d90dc
Mafaz Manzil <90832527+Mafaz5128@users.noreply.github.com>
2025-01-16 15:56:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -403,9 +403,9 @@ if st.sidebar.button('Generate: Average Fare, and Pax Trends'):
 year_type = st.sidebar.selectbox("Select Year Type", ["LY", "TY"])
 snap_date_name = st.sidebar.selectbox("Select Snap Date", ['29-Dec', '22-Dec', '15-Dec', '08-Dec', '01-Dec', '24-Nov', '17-Nov', '10-Nov', '03-Nov'])
 a1, a2 =st.columns(2)
-a1.sidebar.header('Region Wise Metrics-This Year')
+a1.header('Region Wise Metrics-This Year')
 month = a1.sidebar.selectbox("Select Month -TY", df['Month'].unique())
-a2.sidebar.header('Region Wise Metrics-Last Year')
+a2.header('Region Wise Metrics-Last Year')
 MonthM_LY = a2.sidebar.selectbox('Actual Metrics of LY - Select Month:', monthm_ly_options)
 
 if st.sidebar.button('Generate : Region Wise Metrics'):
